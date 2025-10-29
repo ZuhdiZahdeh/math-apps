422b7cde59ed1e646eeedfd3422556001c66650a
Zuhdi Zahdeh <zuhdi.zahdeh@gmail.com>
2025-10-29 12:59:35 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,11 @@
+// أدوات بسيطة مشتركة
+export const qs  = (sel, el=document)=> el.querySelector(sel);
+export const qsa = (sel, el=document)=> [...el.querySelectorAll(sel)];
+export const byId = (id)=> document.getElementById(id);
+
+export const debounce = (fn, ms=250)=>{
+  let t; return (...args)=>{ clearTimeout(t); t=setTimeout(()=>fn(...args), ms); };
+};
+
+export const norm = (s)=> (s||"").toString().toLowerCase();
 
